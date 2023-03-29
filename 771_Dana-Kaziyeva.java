6af3ec79aa1d65f3690e04145ed9c74723615a34
Dana-Kaziyeva <119665823+Dana-Kaziyeva@users.noreply.github.com>
2023-03-29 17:37:46 +0600

    Delete 771_Dana-Kaziyeva.java

--- 771_Dana-Kaziyeva.java
+++
@@ -1,9 +0,0 @@
-class Solution {
-    public int numJewelsInStones(String s1, String s2) {//jewels, stones
-        int ans = 0;
-        for (int i = 0; i < s2.length(); i++) {
-            if (s1.indexOf(s2.charAt(i)) != -1) ans++;
-        }
-        return ans;
-    }
-}
