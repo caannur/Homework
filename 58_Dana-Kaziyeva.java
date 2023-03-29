62f178a382fbf90a9008a7bff4d71a5b2c631e58
Dana-Kaziyeva <119665823+Dana-Kaziyeva@users.noreply.github.com>
2023-03-29 17:39:57 +0600

    Delete 58_Dana-Kaziyeva.java

--- 58_Dana-Kaziyeva.java
+++
@@ -1,9 +0,0 @@
-class Solution {
-    public int lengthOfLastWord(String s) {
-        s=s.trim();
-        int i=0;
-        while(s.length()-1-i!=-1 && s.charAt(s.length()-1-i)!=' ')
-            i++;
-        return i;     
-    }
-}
