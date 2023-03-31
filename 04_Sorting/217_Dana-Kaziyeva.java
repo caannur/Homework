6d2b558eeda956747cff8e31ae19fbffff2ea498
Dana-Kaziyeva <119665823+Dana-Kaziyeva@users.noreply.github.com>
2023-03-31 17:47:40 +0600

    Create 217_Dana-Kaziyeva.java

---
+++ 04_Sorting/217_Dana-Kaziyeva.java
@@ -0,0 +1,9 @@
+class Solution {
+    public boolean containsDuplicate(int[] nums) {
+        Set<Integer> set = new HashSet<>();
+        for(int i:nums)
+            set.add(i);
+        if(set.size()!= nums.length)return true;
+        else return false;
+    }
+}
