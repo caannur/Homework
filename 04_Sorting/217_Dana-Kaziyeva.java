0b745f453b0deb0517b9f017bb0b279da2c4d656
Dana-Kaziyeva <119665823+Dana-Kaziyeva@users.noreply.github.com>
2023-03-31 17:49:51 +0600

    Update 217_Dana-Kaziyeva.java

--- 04_Sorting/217_Dana-Kaziyeva.java
+++ 04_Sorting/217_Dana-Kaziyeva.java
@@ -1,8 +1,10 @@
 class Solution {
     public boolean containsDuplicate(int[] nums) {
         Set<Integer> set = new HashSet<>();
-        for(int i:nums)
+        for(int i:nums){
+            if(set.contains(i))return true;
             set.add(i);
+        }
         if(set.size()!= nums.length)return true;
         else return false;
     }
